05950c15957dd8a883e3bfc896ec8bf939cbc146
paisely65 <cunysps@gmail.com>
2016-04-07 18:23:55 -0400

    Task3 1st revision.

---
+++ task_03.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module creates subclasses."""
+
+import produce
+
+class Apple(produce.Produce):
+    """An object which stores the apples produced.
+Arguments:
+    Produce(object): Amount of produce
+Attribute:
+    duration(integer): Duration for apple production
+"""
+duration = 5356800
