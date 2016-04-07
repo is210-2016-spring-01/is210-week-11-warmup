ef6a81d07797cd859c81c4b75961e4097723db61
paisely65 <cunysps@gmail.com>
2016-04-07 17:44:11 -0400

    Task2 revised-spacing.

--- task_02.py
+++ task_02.py
@@ -5,21 +5,21 @@
 import time
 
 class Snapshot(object):
-
-    
     """This class produces a Unix Timestamp.
 
     Attributes: None
     """
 
     def __init__(self):
-        """A constructor for the class Snapshot
+
+
+        """A constructor for the class Snapshot.
 
         Arguments:
             time(mix): A timestamp from a Unix clock
 
         Attributes:
-            time(mix): A time span from a Inix clock
+            time(mix): A time span from a Unix clock
         """
 
         self.created = time.time()
