cfaaa81f0bd4236dec154ed81d89f2494543196c
paisely65 <cunysps@gmail.com>
2016-04-07 15:23:59 -0400

    Task 1 is finished.

---
+++ task_01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Starter data module to access classes"""
+
+import produce
+
+class Produce(object):
+    """A produce class"""
+
+    def __init__(self, TOMATO, EGGPLANT=1311210802):
+        """This class analyzes produce."""
+        self.TOMATO = TOMATO_ARRIVAL
+
+    def get_expiration(self):
+        """This class  shows the expiration date."""
+        self.EGGPLANT = EGGPLANT_EXPIRES
