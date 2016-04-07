631a6a5f29d1214c32891ee9bc84ad32fb11f394
paisely65 <cunysps@gmail.com>
2016-04-07 18:34:19 -0400

    Task 3 revision 2.

--- task_03.py
+++ task_03.py
@@ -4,11 +4,13 @@
 
 import produce
 
+
 class Apple(produce.Produce):
     """An object which stores the apples produced.
-Arguments:
-    Produce(object): Amount of produce
-Attribute:
-    duration(integer): Duration for apple production
-"""
-duration = 5356800
+    Arguments:
+        Produce(object): Amount of produce
+    Attribute:
+        duration(int): Duration for apple production
+    """
+    
+    duration = 5356800
