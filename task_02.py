5a79b307f2312934834bda4977432bd1323e3cac
paisely65 <cunysps@gmail.com>
2016-04-07 16:29:46 -0400

    Iask 2 is finished.

---
+++ task_02.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module provides functions specifically related to time."""
+
+import time
+
+class Snapshot(object):
+    """This class produces a Unix Timestamp.
+
+    Attributes: None
+    """
+
+    def __init__ (self):
+        """A constructor for the class Snapshot
+
+        Arguments:
+            time(mix): A timestamp from a Unix clock
+
+        Attributes:
+            time(mix): A time span from a Inix clock
+        """
+
+        self.created = time.time()
+
+
