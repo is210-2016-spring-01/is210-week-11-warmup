7604df804c8eacc4e760897a550029becb56591e
paisely65 <cunysps@gmail.com>
2016-04-07 21:02:15 -0400

    Task 4

--- task_04.py
+++ task_04.py
@@ -6,42 +6,27 @@ import car
 
 
 class CustomCar(car.Car):
-    """Another version of the car class.
+    """A childclass of carCar."""
+
+def __init__(self, color='pink', tires=None):
+    """ Constructor for custom car
+    
+    Args:
+        tires (list): list of CustomTire objects.
 
     Attributes:
-        None.
+        tires: if none will append up to 8
     """
-class CustomTire(car.Tire):
-    """A subclass of Tire for CustomTire
+    car.Car.__init__(self, color)
 
-    Attributes:
-        None.
-    """
-
-    def __init__(self, cars = 1, miles=0, tires = 4, color = None,):
-        """The class creator for CustomCar.
-
-        Args:
-            color (string): color of car
-            cars( int): Cars to be created. Default = 1
-            tires(list): list of CustomTire objects (equal to 4)
-            miles(int): distance cars travel
-
-        Attributes:
-            tires(list): list of CustomTire objects (equal to 4)
-        """
     if tires is None:
-        self.tires = []
-    for i in xrange(4):
-                self.tires.append(CustomTire(i-i))
-    else:
-        self.tires = tires
+        tires = []
+        while len(tires) < 4:
+            Tires.append(CustomTire())
+    self.tires = tires
 
-        Args:
-            miles(int): The number of miles on tire. Default is 0.
-            tires(int): The number of tires on the car
 
-        Attributes:
-            miles(int): The number of miles on the tire.
-        """
-        self.__maximum_miles = 500
+class CustomTire(car.Tire):
+    """Child class of car.Tire."""
+    __maximum_miles = 500
+    
