88f4a06b505d3e4a011a87c3f4abe19570b8acdd
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-04-08 08:20:00 +0000

    ehh

---
+++ task_03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 03: class attributes"""
+
+
+import produce
+
+class Apple(produce.Produce):
+    """update class attribute"""
+    duration = 5356800
---
+++ task_04.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 04"""
+
+
+import car
+
+class CustomCar(car.Car):
+    """a child class of car.Car"""
+    def __init__(self, color=None, tires=None):
+        """constructor. argument/attribute: tires.
+        returns a list of car tires."""
+        self.tires = tires
+        car.Car.__init__(self, tires)
+        self.tires = tires
+        tires = CustomTire()
+        if self.tires is None:
+            self.tires = []
+            while len(self.tires) < 4:
+                self.tires.append(tires)
+                
+
+class CustomTire(car.Tire):
+    """a child class of car.Tire"""
+    def __init__(self, miles=0):
+        """constructor"""
+         self.__maximum_miles = 500
+
