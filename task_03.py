b9e8a347413dde2852be7c8d0542597861abe12b
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-04-08 03:08:07 +0000

    Subclassed existing class

---
+++ task_03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A small docstring for a subclass"""
+
+import produce
+
+
+class Apple(produce.Produce):
+    """A docstring for subclassing an existing class.
+
+    Returns numeric value or duration of specific produce.
+
+    Args:
+
+    Attributes:
+        duration (numeric): Duration of the produce.
+    """
+    duration = 5356800
