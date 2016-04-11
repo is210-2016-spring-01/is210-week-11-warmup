ad7cd7cca3d5959492e502f2ec3ce3dbf136bca6
jaiz25 <jaizelle@gmail.com>
2016-04-11 03:31:17 +0000

    task_3

---
+++ task_03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+"""Task 3 docstring."""
+
+
+import produce
+
+
+class Apple(produce.Produce):
+    """Subclassed from produce.Produce that stores Apple data.
+
+    Attributes:
+        duration (int): The duration of Apple.
+    """
+
+    duration = 5356800
